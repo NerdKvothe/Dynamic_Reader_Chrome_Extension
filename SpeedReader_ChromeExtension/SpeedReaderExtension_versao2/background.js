1aaf2f3fabf0a9443f2184cf838c84b45ad3fe44
Smaily Carrilho <smaily.prado@gmail.com>
2014-04-17 11:05:01 -0400

    Melhorias

--- SpeedReader_ChromeExtension/SpeedReaderExtension_versao2/background.js
+++ SpeedReader_ChromeExtension/SpeedReaderExtension_versao2/background.js
@@ -9,65 +9,48 @@ chrome.runtime.onInstalled.addListener(function() {
             });  
 });
 
-
-function onClickHandler(info, tab) {
-
-    //alert( info.selectionText );
-
-   chrome.windows.getCurrent(function(curWindow) {
-      chrome.windows.create(
-          {"url": "background.html",
-           "focused": false,
-           "top": Math.round(curWindow.top + 3/10 * curWindow.height),
-           "left": Math.round(curWindow.left + 3/10 * curWindow.width),
-           "width": Math.round(3/10 * curWindow.width ),
-           "height": Math.round(3/10 * curWindow.height)
-           },
-          function(newWindow) {
-            readerId = newWindow.id;
-            readerWindow = chrome.extension.getViews({"windowId": readerId})[0];
-
-            curOptions = options;
-            logOptions();
-
-            // Fastest timeout == 1 ms (@ options.rate = 10.0)
-            milliseconds = 10 / curOptions.rate;
-            logUtterance(utterance, 0, sendReaderEvent);
-          }
-        );
-    });
+function displayWords(selectionText){
+    var textToRead = selectionText;
+    var counter = 0;
+    var splittedText = textToRead.split(" ");
     
-    (function($){
-    
-    reader = {};
-    reader.readerEvent = null;
-    reader.textToRead = info.selectionText;
-    reader.counter = 0;
-    
-    
-    reader.init = function() {
-        startReadingBtn = $("#start-reading-btn");
-        startReadingBtn.bind("click", reader.startReading);
-    },
-    
-    reader.startReading = function() {
-        var overlay = $("#overlay");
-        var closeOverlay = $("#closeOverlay");
-        var content = $("#content");
-       
-       closeOverlay.unbind().bind("click", function(){
-           reader.stopReadingEvent();
-           closeOverlay.toggle();
-           overlay.toggle();
-           content.removeClass("blur");
-        });
+    if (counter < splittedText.length) {
+        var wordToShow = splittedText[counter++];
+        alert(wordToShow);
+        document.getElementById('#wordToRead').innerHTML= "'" + wordToShow + "'";
+        alert(selectionText);
+        document.getElementById('#wordToRead').innerHTML= "";
+        return wordToShow;
+    }
+    /*
+    function splitWord(word) {
+        var middleRedCharPosition = null;
+        var leftRedCharPosition = null;
+        var rightRedCharPosition = null;
+        var isCharsNumberAPair = word.length % 2 === 0;
+        var splittedWord = word.split(" ");
         
-        overlay.toggle();
-        closeOverlay.toggle();
-        content.addClass("blur");
+        if (isCharsNumberAPair) {
+            leftRedCharPosition = (word.length / 2) - 1;
+            rightRedCharPosition = leftRedCharPosition + 1;
             
-        reader.startReadingEvent();
+            for (var i = 0 ; i < splittedWord.length ; i++) {
+                var span = (i === leftRedCharPosition || i === rightRedCharPosition) ? $("<span/>").addClass("red") : $("<span/>");
+                span.text(splittedWord[i]);
+                spanContainer.append(span);
+            }
+        } else {
+            middleRedCharPosition = (word.length - 1) / 2;
+            
+            for (var i = 0 ; i < splittedWord.length ; i++) {
+                var span = (i === middleRedCharPosition) ? $("<span/>").addClass("red") : $("<span/>");
+                span.text(splittedWord[i]);
+                spanContainer.append(span);
+            }
+        }
+        return spanContainer;
     };
+    
     
     reader.startReadingEvent = function() {
         reader.textToRead = $("#textToRead").val().split(/\s/);
@@ -124,9 +107,45 @@ function onClickHandler(info, tab) {
         return spanContainer;
     };
 })(jQuery);;
+*/
+};
 
-    
-    
+
+function onClickHandler(info, tab) {
+
+    //alert( info.selectionText );
+
+   chrome.windows.getCurrent(function(curWindow) {
+      chrome.windows.create(
+          {"url": "background.html",
+           "focused": false//,
+           //"top": Math.round(curWindow.top + 3/10 * curWindow.height),
+           //"left": Math.round(curWindow.left + 3/10 * curWindow.width)//,
+           //"width": Math.round(3/10 * curWindow.width ),
+           //"height": Math.round(3/10 * curWindow.height)
+           },
+          function(newWindow) {
+            readerId = newWindow.id;
+            readerWindow = chrome.extension.getViews({"windowId": readerId})[0];
+
+            curOptions = options;
+            logOptions();
+
+            // Fastest timeout == 1 ms (@ options.rate = 10.0)
+            milliseconds = 10 / curOptions.rate;
+            logUtterance(utterance, 0, sendReaderEvent);
+          }
+        );
+        
+        //var selectionedText = info.selectionText;
+        
+        chrome.tabs.query({ active: false, windowId: chrome.windows.WINDOW_ID_CURRENT }, function (tabs) {
+            //alert(info.selectionText);
+            displayWords(info.selectionText);
+        });
+        
+    });
+
 };
 
 // add click event
